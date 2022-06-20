ff1144d6f2ed0098223e49aef134e1d2be00f22a
pys890102 <pys890102@bkwinnners.com>
2022-06-20 10:54:42 +0900

    차액정산 VANID 표시

--- src/com/pgmate/dm/main/KsnetDiffTrxDownLoad.java
+++ src/com/pgmate/dm/main/KsnetDiffTrxDownLoad.java
@@ -113,18 +113,18 @@ public class KsnetDiffTrxDownLoad {
 						logger.info("updateTrxCap [{}]",dao.updateTrxCap(nowDate));
 					}
 					
-					String msgBody = day + " KSNET 차액정산 " + formatter.format(list.size()) + "건 완료.";
+					String msgBody = day + " KSNET 차액정산"+ "("+id+") " + formatter.format(list.size()) + "건 완료.";
 					smsGw.sendMessage("0", "3", msgBody);
 				} else {
-					String msgBody = day + " KSNET 차액정산 다운로드 파일이 존재하지 않습니다.";
+					String msgBody = day + " KSNET 차액정산 다운로드 파일이 존재하지 않습니다." + "("+id+")";
 					smsGw.sendMessage("0", "3", msgBody);
 				}
 				
 				logger.info("============= "+nowDate+"일자 차액정산 종료 ===============");
 			}catch (Exception e) {
-				String msgBody = day + " KSNET 차액정산 오류. 확인요망";
+				String msgBody = day + " KSNET 차액정산 오류. 확인요망"+"("+id+")";
 				smsGw.sendMessage("0", "3", msgBody);
-				
+			
 				logger.error(e.getMessage(), e);
 			}	
 		}

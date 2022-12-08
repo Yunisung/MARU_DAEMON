1ff72505d36ebbf9ad6ffcd68a8ca6857146a5a2
pys890102 <pys890102@bkwinners.com>
2022-12-08 11:10:00 +0900

    월정산 1일로 변경

--- src/com/pgmate/dm/main/Settle.java
+++ src/com/pgmate/dm/main/Settle.java
@@ -63,7 +63,7 @@ public class Settle {
 			}
             	
 			// 월정산(영업대행)
-			if("05".equals(cmd.substring(6))){
+			if("01".equals(cmd.substring(6))){
 				monthExecute(cmd);
 				monthVactExecute(cmd); // 가상계좌
 

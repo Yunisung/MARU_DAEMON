2347cb4d75660d68c647b7dc87b9434386edf672
pys890102 <pys890102@bkwinners.com>
2023-03-13 14:03:51 +0900

    가상계좌만료시 만료시간 다시 세팅

--- src/com/pgmate/dm/dao/VactAuthDAO.java
+++ src/com/pgmate/dm/dao/VactAuthDAO.java
@@ -365,6 +365,8 @@ public class VactAuthDAO extends DAO {
     public boolean updateVactDtlReady(String issueId, String holderName) {
         super.setTable("PG_VACT_DTL");
 
+        //230313_PYS : 만료기간 갱신
+        String expire = (Integer.parseInt(CommonUtil.getCurrentDate("yyyy")) + 1) + CommonUtil.getCurrentDate("MMdd")+"00";
         super.setRecord("status", "대기");
         super.setRecord("amount", 0);
         super.setRecord("oper", "ge");
@@ -374,6 +376,8 @@ public class VactAuthDAO extends DAO {
         super.setRecord("transferKey", "");
         super.setRecord("udf1", "");
         super.setRecord("udf2", "");
+        super.setRecord("expireAt", expire);
+        super.setRecord("expireDate", null);
         super.setRecord("regDay", 	CommonUtil.getCurrentDate("yyyyMMdd"));
         super.addWhere("issueId", 	issueId);
         boolean update = super.update();

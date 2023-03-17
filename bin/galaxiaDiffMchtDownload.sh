eebfc1aae7bd0ced6f0b42604874e635e74b591a
kjm <jjggooo>
2023-03-17 14:49:33 +0900

    갤럭시아 차액정산 스크립트 추가

---
+++ bin/galaxiaDiffMchtDownload.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cd /home/bkwinners/MARU_DAEMON/bin
+
+# SET LIBRARY
+for i in ../lib/*.jar; do
+    CP=$CP:$i
+done
+CP=`echo $CP | cut -c2-`
+
+
+# JVM_ARGS for VM
+##########################
+JVM_ARGS="-DMARU_WEBHOOK -DCP_CONF=../conf -Dlogback.configurationFile=../conf/logback.xml -Dfile.encoding=UTF-8"
+JVM_ARGS="$JVM_ARGS -Xss512k -Xms64m -Xmx128m"
+JVM_ARGS="$JVM_ARGS -cp $CP:../classes"
+
+TARGET_DATE="$(date +%Y%m%d)"
+#TARGET_DATE="20210704"
+
+java $JVM_ARGS com.pgmate.dm.main.GalaxiaDiffMchtDownLoad $TARGET_DATE

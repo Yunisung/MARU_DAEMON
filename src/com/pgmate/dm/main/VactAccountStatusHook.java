fb9f68e4a06557c9e11cb66960c9716c165f86dc
pys890102 <pys890102@bkwinners.com>
2024-01-25 13:19:26 +0900

    240125_가상계좌 노티 오류 수정

--- src/com/pgmate/dm/main/VactAccountStatusHook.java
+++ src/com/pgmate/dm/main/VactAccountStatusHook.java
@@ -63,6 +63,7 @@ public class VactAccountStatusHook extends Thread {
             conn.setDoOutput(true);
             conn.setRequestMethod("POST");
             conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+            conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
             conn.setConnectTimeout(10000);
             conn.setReadTimeout(10000);
             OutputStream os = conn.getOutputStream();

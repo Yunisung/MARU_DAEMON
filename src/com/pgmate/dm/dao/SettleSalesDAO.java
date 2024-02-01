cb91741ce6230d9e11ebaeffa751802afcfb56d6
kjm <jjggooo>
2024-02-01 11:16:07 +0900

    월세앱 - 일반 정산 쿼리에서 월세앱 제외 추가

--- src/com/pgmate/dm/dao/SettleSalesDAO.java
+++ src/com/pgmate/dm/dao/SettleSalesDAO.java
@@ -34,7 +34,7 @@ public class SettleSalesDAO extends SettleDAO implements ImplSettle {
 				+"  ,SUM(IF(capType ='매입', 0,stlFeeVat)) rfdVat "
 				+"  ,SUM(stlSalesFee) stlFee "								// 정산 금액 (수수료를 정산)
 				+"  ,SUM(stlDiffSalesFee) stlDiffFee "						// 영중소 차액정산 수수료 정산 금액
-				+"  FROM VW_TRX_CAP_LIST where stlSalesDay between '"+month+"01' and '"+month+"31' and stlSalesId ='' group by salesId, stlSalesDay "
+				+"  FROM VW_TRX_CAP_LIST where stlSalesDay between '"+month+"01' and '"+month+"31' and serviceType != '월세앱' and stlSalesId ='' group by salesId, stlSalesDay "
 				+"  ) A , PG_MAM_SALES_MNG  B WHERE A.salesId = B.salesId  ";
 		RecordSet rset = super.query(q);
 		super.initRecord();

1c8127ea3f9dc35230845a0af0c66b077ae84ff7
jjggoo <kjm7943@bkwinners.com>
2025-04-10 17:53:56 +0900

    가상계좌 리스크 > 스크립트 추가

---
+++ bin/vactRiskCheck.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cd /var/lib/jenkins/workspace/MARU_DAEMON/bin
+
+# SET LIBRARY
+for i in ../lib/*.jar; do
+    CP=$CP:$i
+done
+CP=`echo $CP | cut -c2-`
+
+
+# JVM_ARGS for VM
+##########################
+JVM_ARGS="-DMARU_VACT_RISK_CHECK -DCP_CONF=../conf -Dlogback.configurationFile=../conf/logback.xml -Dfile.encoding=UTF-8"
+JVM_ARGS="$JVM_ARGS -Xss512k -Xms64m -Xmx128m"
+JVM_ARGS="$JVM_ARGS -cp $CP:../classes"
+
+
+java $JVM_ARGS com.pgmate.dm.main.VactRiskCheck
